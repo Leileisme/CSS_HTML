3d4d4f3843600ea734df6bdda7499c167a8fbf1a
Leileisme <onlymymind1108@gmail.com>
2024-01-09 16:14:29 +0800

    1/9 scss+gsap 整合網頁(上課檔案)
    1/9 scss+gsap 整合網頁(上課檔案)

---
+++ 20240109/0.課程資料/js/all.js
@@ -0,0 +1,50 @@
+$('#reg_btn, #login_btn').on('click', function () {
+	$('body, #navbar').css({
+		overflow: 'auto',
+		'padding-right': 0
+	})
+})
+
+$('#race a').on('click', function () {
+	$('#race a').removeClass('active')
+	$(this).addClass('active')
+})
+
+// swiper ---------------------------------------------------------------------------------------------
+new Swiper('.swiper', {
+	direction: 'horizontal',
+	loop: true,
+	speed: 1000,
+	spaceBetween: 15,
+	centeredSlides: true,
+	autoplay: {
+		delay: 5000
+	},
+	slidesPerView: 1,
+	effect: 'coverflow',
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true
+	},
+	breakpoints: {
+		576: {
+			slidesPerView: 2
+		},
+		768: {
+			slidesPerView: 3
+		},
+		920: {
+			slidesPerView: 3
+		},
+		1200: {
+			slidesPerView: 4
+		}
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+})
